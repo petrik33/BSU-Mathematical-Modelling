28357101a7d9075e5992e83b9b2533ddc680eabb
Timofey Petrikevich <petrik3003@gmail.com>
2023-10-05 13:31:50 +0300

    feat: implement moments test

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -61,6 +61,53 @@ impl RandGenerator for McLarenMarsagliaMethod {
     }
 }
 
+struct RandSet {
+    generator: Box<dyn RandGenerator>,
+    data: Vec<f64>,
+}
+
+impl RandSet {
+    fn new(mut generator1: Box<dyn RandGenerator>) -> RandSet {
+        RandSet {
+            generator: generator1,
+            data: Vec::new(),
+        }
+    }
+
+    fn reserve(&mut self, size: usize) {
+        self.data.reserve(size);
+    }
+
+    fn push(&mut self) {
+        self.data.push(self.generator.rand());
+    }
+}
+
+#[derive(Debug)]
+struct RandMoments {
+    mean : f64,
+    variance : f64,
+    skewness : f64,
+    kurtosis : f64
+}
+
+fn calculate_moments(data: &[f64]) -> RandMoments {
+    let n = data.len() as f64;
+    let mean = data.iter().sum::<f64>() / n;
+    let variance = data.iter().map(|x| (x - mean).powi(2)).sum::<f64>() / n;
+    let skewness = data.iter().map(|x| (x - mean).powi(3)).sum::<f64>() / (n * variance.powf(1.5));
+    let kurtosis =
+        data.iter().map(|x| (x - mean).powi(4)).sum::<f64>() / (n * variance.powi(2)) - 3.0;
+
+    RandMoments {mean, variance, skewness, kurtosis}
+}
+
+fn test_moments(a : &RandMoments, b : &RandMoments, significance : f64) -> bool {
+    (a.mean - b.mean).abs() <= significance
+        && (a.variance - b.variance).abs() <= significance
+        && (a.skewness - b.skewness).abs() <= significance
+        && (a.kurtosis - b.kurtosis).abs() <= significance
+}
 
 fn main() {
     const A1: u32 = 564853681;
@@ -76,22 +123,70 @@ fn main() {
         K,
     );
 
-    println!("First random number from MultiplicativeCongruentialMethod: {}", rng1.rand());
-    println!("First random number from McLarenMarsagliaMethod: {}", rng2.rand());
+    let mut random_set1 = RandSet::new(Box::new(rng1));
+    let mut random_set2 = RandSet::new(Box::new(rng2));
 
-    for _ in 1..14 {
-        rng1.rand();
-        rng2.rand();
+    random_set1.reserve(1000);
+    random_set2.reserve(1000);
+
+    random_set1.push();
+    random_set2.push();
+
+    println!(
+        "First random number from MultiplicativeCongruentialMethod: {}",
+        random_set1.data[0]
+    );
+    println!(
+        "First random number from McLarenMarsagliaMethod: {}",
+        random_set2.data[0]
+    );
+
+    for _ in 1..15 {
+        random_set1.push();
+        random_set2.push();
     }
 
-    println!("15th random number from MultiplicativeCongruentialMethod: {}", rng1.rand());
-    println!("15th random number from McLarenMarsagliaMethod: {}", rng2.rand());
+    println!(
+        "15th random number from MultiplicativeCongruentialMethod: {}",
+        random_set1.data[14]
+    );
+    println!(
+        "15th random number from McLarenMarsagliaMethod: {}",
+        random_set2.data[14]
+    );
 
-    for _ in 15..999 {
-        rng1.rand();
-        rng2.rand();
+    for _ in 15..1000 {
+        random_set1.push();
+        random_set2.push();
     }
 
-    println!("Last (1000th) random number from MultiplicativeCongruentialMethod: {}", rng1.rand());
-    println!("Last (1000th) random number from McLarenMarsagliaMethod: {}", rng2.rand());
+    println!(
+        "Last (1000th) random number from MultiplicativeCongruentialMethod: {}",
+        random_set1.data[999]
+    );
+    println!(
+        "Last (1000th) random number from McLarenMarsagliaMethod: {}",
+        random_set2.data[999]
+    );
+
+    let tested_moments1 = calculate_moments(&random_set1.data);
+    let tested_moments2 = calculate_moments(&random_set2.data);
+    const theorethical_moments : RandMoments = RandMoments {
+        mean: 0.5,
+        variance: 0.0833,
+        skewness: 0.0,
+        kurtosis: -1.2
+    };
+
+    const significance1 : f64 = 0.05;
+
+    println!(
+        "Moments test for MultiplicativeCongruentialMethod: {:?} vs {:?}. Test completed: {}",
+        tested_moments1, theorethical_moments, test_moments(&tested_moments1, &theorethical_moments, significance1)
+    );
+
+    println!(
+        "Moments test for McLarenMarsagliaMethod: {:?} vs {:?}. Test completed: {}",
+        tested_moments2, theorethical_moments, test_moments(&tested_moments1, &theorethical_moments, significance1)
+    );
 }
